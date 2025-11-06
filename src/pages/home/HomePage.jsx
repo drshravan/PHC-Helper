74b6329a446a097149e90983bf4394146a7e68b4
siddulashravan7 <siddulashravan7@gmail.com>
2025-11-06 16:20:23 +0000

    s

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -5,20 +5,25 @@ import { Link } from "react-router-dom"
 
 
 
-export default function HomePage() {
-    return (
-      <div style={{ 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        textAlign: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-        }}>
 
-                <Link className="link" to= "/dogbite"> Dog Bite</Link>
-                <Link className="link" to= "/Eddlist"> EDD List</Link>
-        
+export default function HomePage() {
+  return (
+    <div className="home-container">
+      <div className="home-card">
+        <h1 className="home-title">🏥 PHC Helper</h1>
+        <p className="home-subtitle">Choose an option below to continue</p>
+
+        <div className="home-links">
+          <Link className="home-link" to="/dogbite">
+            🐶 Dog Bite Management
+          </Link>
+          <Link className="home-link" to="/Eddlist">
+            🤰 EDD List
+          </Link>
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
+
   
