f8e523e36ed526d1c50b6a9e015d48db644160cb
siddulashravan7 <siddulashravan7@gmail.com>
2025-11-06 14:13:32 +0000

    s

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PHC-Helper/', // 👈 This must match your repo name exactly (case-sensitive)
+  base: '/PHC-Helper/', // 👈 must match your GitHub repo name exactly (case-sensitive)
 })
